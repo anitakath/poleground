489d26f8e80cd077250f8c2d85c9260709b9586d
Anne Wagner <wagner.annekathrin@gmx.de>
2025-03-22 20:26:36 +0100

    responsive design at Header and Navigation - theres one navigation bar for phones and one for web pages

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,27 +3,63 @@ import Link from "next/link";
 import styles from './Layout.module.css';
 import Image from "next/image";
 import { useTitle } from "../../context/TitleContext";
-
+import { useState } from "react";
 
 const Header = () => {
 
     const { title, setTitle } = useTitle();
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
 
   
   
     return (
         <div className={`min-h-20 flex relative items-center justify-center px-4 ${styles.container}`}>
-            <nav className="flex w-full my-2 justify-center items-center space-x-4">
-                <Link href="/about" className={styles.link}> FAQ </Link>
-                <Link href="/about" className={styles.link}> ÜBER UNS </Link>
-                <div className="text-lg px-4 relative font-bold">
+           
+            <nav className={styles.navigation}>
+                <div className={styles.titleContainer}>
                     <Link href="/"> {title} </Link>
                 </div>
-                <Link href="/events" className={styles.link}> EVENTS </Link>
-                <Link href="/kooperationen" className={styles.link}> KOOPERATIONEN </Link>
+
+                <div>
+                    <Link href="/about" className={styles.link}> FAQ </Link>
+                    <Link href="/about" className={styles.link}> ÜBER UNS </Link>
+                    <Link href="/events" className={styles.link}> EVENTS </Link>
+                    <Link href="/kooperationen" className={styles.link}> KOOPERATIONEN </Link>
+                </div>
+
+                <div className={styles.mobileMenu}>
+                    <button onClick={toggleMenu} className={styles.menuButton}> 
+                        <Image 
+                            src="/iconpng/icons8-menü-50.png" 
+                            alt="menu button" 
+                            width={30} 
+                            height={30}
+                            
+                        />    
+                    </button>
+                   
+                </div>
+                <div className={`${styles.mobileLinksContainer} ${isOpen ? styles.open : ''}`}>
+                    <div className={styles.closeMobileMenuButton}> 
+                        <button onClick={toggleMenu}>
+                            schließen
+                            {/*<Image src="/iconpng/icons8-löschen-50.png" alt="close button image" width={50} height={50} />*/}
+                        </button> 
+                    </div>
+                    <Link href="/about" className={styles.mobileLink}>FAQ</Link>
+                    <Link href="/about" className={styles.mobileLink}>ÜBER UNS</Link>
+                    <Link href="/events" className={styles.mobileLink}>EVENTS</Link>
+                    <Link href="/kooperationen" className={styles.mobileLink}>KOOPERATIONEN</Link>
+                </div>
+
             </nav>
+
             <div className={styles.socialContainer}>
                 <button className={styles.socialButton}>
                     <Image alt="button to connect with us via instagram" src="/iconpng/icons8-instagram-50.png" width={40} height={40} className={styles.socialIcon}/>
